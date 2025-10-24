813e727071117b83214cd6506b2ed51021509238
bomkoasya <bomko.asya0102@gmail.com>
2025-10-24 18:50:21 +0300

    test: forecast

--- tests/test_forecast.cpp
+++ tests/test_forecast.cpp
@@ -2,63 +2,65 @@
 #include "../Forecast.h"
 #include "../ForecastModel.h"
 #include "../Transaction.h"
+#include <filesystem>
 #include <vector>
 
-TEST(ForecastTest, PredictNext) {
+class ForecastTestFixture : public ::testing::Test {
+protected:
     Forecast forecast;
+};
+
+class ForecastModelTestFixture : public ::testing::Test {
+protected:
+    ForecastModel model;
+};
+
+TEST_F(ForecastTestFixture, GIVEN_Data_WHEN_PredictNextCalled_THEN_ReturnsAverageDifference) {
     forecast.addData(10.0);
     forecast.addData(20.0);
     forecast.addData(30.0);
-    EXPECT_DOUBLE_EQ(forecast.predictNext(), 20.0);
+    EXPECT_NEAR(forecast.predictNext(), 20.0, 1e-9);
 }
 
-TEST(ForecastTest, PredictNextEmpty) {
-    Forecast forecast;
+TEST_F(ForecastTestFixture, GIVEN_EmptyData_WHEN_PredictNextCalled_THEN_ReturnsZero) {
     EXPECT_DOUBLE_EQ(forecast.predictNext(), 0.0);
 }
 
-TEST(ForecastModelTest, ForecastNextMonth) {
-    ForecastModel model;
-    std::vector<Transaction> txs;
-    txs.push_back(Transaction{"", 10.0, 0, "", "cat1"});
-    txs.push_back(Transaction{"", 20.0, 0, "", "cat1"});
-    EXPECT_DOUBLE_EQ(model.forecastNextMonth(txs), 450.0);
+TEST_F(ForecastModelTestFixture, GIVEN_Transactions_WHEN_ForecastNextMonthCalled_THEN_ReturnsExpectedValue) {
+    std::vector<Transaction> txs = {
+        {"", 10.0, 0, "", "cat1"},
+        {"", 20.0, 0, "", "cat1"}
+    };
+    double expected = (10.0 + 20.0) / 2 * 30;
+    EXPECT_NEAR(model.forecastNextMonth(txs), expected, 1e-9);
 }
 
-TEST(ForecastModelTest, ForecastNextMonthEmpty) {
-    ForecastModel model;
+TEST_F(ForecastModelTestFixture, GIVEN_EmptyTransactions_WHEN_ForecastNextMonthCalled_THEN_ReturnsZero) {
     std::vector<Transaction> txs;
     EXPECT_DOUBLE_EQ(model.forecastNextMonth(txs), 0.0);
 }
 
-TEST(ForecastModelTest, RollingAverage) {
-    ForecastModel model;
+TEST_F(ForecastModelTestFixture, GIVEN_Values_WHEN_RollingAverageCalled_THEN_ReturnsCorrectAverage) {
     std::vector<double> values = {10.0, 20.0, 30.0, 40.0, 50.0};
-    EXPECT_DOUBLE_EQ(model.rollingAverage(values, 3), 40.0);
+    EXPECT_NEAR(model.rollingAverage(values, 3), 40.0, 1e-9);
 }
 
-TEST(ForecastModelTest, RollingAverageEmpty) {
-    ForecastModel model;
+TEST_F(ForecastModelTestFixture, GIVEN_EmptyValues_WHEN_RollingAverageCalled_THEN_ReturnsZero) {
     std::vector<double> values;
     EXPECT_DOUBLE_EQ(model.rollingAverage(values, 3), 0.0);
 }
 
-TEST(ForecastModelTest, RollingAverageWindowLargerThanSize) {
-    ForecastModel model;
+TEST_F(ForecastModelTestFixture, GIVEN_WindowLargerThanSize_WHEN_RollingAverageCalled_THEN_UsesAllValues) {
     std::vector<double> values = {10.0, 20.0, 30.0};
-    EXPECT_DOUBLE_EQ(model.rollingAverage(values, 5), 20.0);
+    EXPECT_NEAR(model.rollingAverage(values, 5), 20.0, 1e-9);
 }
 
-TEST(ForecastModelTest, TrainModel) {
-    ForecastModel model;
+TEST_F(ForecastModelTestFixture, GIVEN_Transactions_WHEN_TrainModelCalled_THEN_DoesNotThrow) {
     std::vector<Transaction> txs;
-    // This is a placeholder, so we just check that it doesn't crash.
-    model.trainModel(txs);
+    EXPECT_NO_THROW(model.trainModel(txs));
 }
 
-TEST(ForecastModelTest, EvaluateForecast) {
-    ForecastModel model;
+TEST_F(ForecastModelTestFixture, GIVEN_Transactions_WHEN_EvaluateForecastCalled_THEN_ReturnsZero) {
     std::vector<Transaction> txs;
-    // This is a placeholder, so we just check that it doesn't crash.
     EXPECT_DOUBLE_EQ(model.evaluateForecast(txs), 0.0);
 }
